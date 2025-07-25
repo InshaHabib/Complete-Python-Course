4be4af6220a464258635ef44b2a6b7d2f006ae49
inshahabib939@gmail.com <inshahabib939@gmail.com>
2025-07-25 23:21:42 +0500

    Updated something

---
+++ Chapter 02 - Variables & Datatypes/04_operators.py
@@ -0,0 +1,38 @@
+
+# Arithmetic Operators :- +, -, *, /
+a = 9
+b = 8
+c = a + b
+print(c)
+
+# Assignment Operators :- =, =+, -=
+a = 4 - 9
+print(a )
+b = 6
+b +=3  # increment 3 in b value
+print(b)
+
+# Comparison Operators :- ==, >, >=, <, !=
+d = 5 > 4
+print(d) # return boolean
+
+e = 10!=10
+print(e)
+
+# Logical Operatprs :-
+f = True or False
+
+# Truth table of 'OR' 
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth table of 'AND'
+print("True and False is ", True and False)
+print("True and True is ", True and  True)
+print("False and True is ", False and True)
+print("False and False is ", False and  False)
+
+print(not(False))   # True
+print(not(True))   # False
---
+++ Chapter 02 - Variables & Datatypes/05_type.py
@@ -0,0 +1,18 @@
+
+a = 30
+b = type(a)
+print(b)
+
+c = 3.34
+d= type(c)
+print(d)
+
+e = "Insha Habib"
+f = type(e)
+print(f)
+
+g = "3.45"
+h =  float(g) # g but the type should be float
+i = type(h)
+print(i)
+  
