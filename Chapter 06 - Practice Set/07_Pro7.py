368f8576d83e935368f358ccb47011f1cee26d4f
inshahabib939@gmail.com <inshahabib939@gmail.com>
2025-08-11 01:21:56 +0500

    Updated Something

--- Chapter 06 - Practice Set/07_Pro7.py
+++ Chapter 06 - Practice Set/07_Pro7.py
@@ -1,3 +1,12 @@
 #  Program to find out whether a given post is talking about "Insha Habib" or not.
 
-# 
+
+# post = "Hey Insha habib is sensitive. Insha habib is a graduate software engineer from FURC. Insha habib goal is to grow as a pythhon developer."
+
+post = input("Enter your post = ")
+
+if("Insha".lower() in post.lower()):
+    print("This post is talking about about insha.")
+
+else:
+    print("There was no mention of insha in this post.")
