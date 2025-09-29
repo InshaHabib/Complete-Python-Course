b0e8eae1384e975a64c86a9c2c78b721b3021476
inshahabib939@gmail.com <inshahabib939@gmail.com>
2025-09-30 04:21:43 +0500

    Updated something

--- Chapter 10 - Practice Set/06_Pro6.py
+++ Chapter 10 - Practice Set/06_Pro6.py
@@ -1,3 +1,22 @@
 # Can you change the self-parameter inside a class to something else say ("Insha"). Try changing self to "slf" or "insha" and see the effects.
+# Answer:- yes, we can change the self parameter to any other name, but it is a convention to use self. Here is an example with "slf" and "insha".
 
+from random import randint
+class train:
+    def __init__(slf, trainNo):
+        slf.trainNo = trainNo
+    
+    def book_ticket(insha, fromm, to):
+        print(f"Ticket is booked in train Number = {insha.trainNo} from {fromm} to {to}")
+        
+    def getstatus(self):
+        print(f"Train number = {self.trainNo} is running on time.")
+    
+    def getfareinfo(self, fromm, to):
+        print(f"Ticket fare in train Number = {self.trainNo} from {fromm} to {to} is {randint(222, 555)}")
+    
+t = train(int(input("Enter train number = ")))
+t.book_ticket("Naushera", "Rawalpindi")
+t.getstatus()
+t.getfareinfo("Naushera", "Rawalpindi")
 
